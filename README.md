02926cb958caf1bba066576a8a04711145005bde
rajput-hemant <hemant.rajput_cs20@gla.ac.in>
2022-06-07 18:10:32 +0530

    init
    Signed-off-by: rajput-hemant <hemant.rajput_cs20@gla.ac.in>

---
+++ README.md
@@ -0,0 +1,19 @@
+![View Counter](https://komarev.com/ghpvc/?username=shop-app&label=View%20Counter&color=red&style=flat) &nbsp; &nbsp; ![Repo Size](https://img.shields.io/github/repo-size/rajput-hemant/shop-app?color=blue)
+
+# Shop App
+
+## Building from Source
+
+- If you don't have Flutter SDK installed, please visit official [Flutter](https://flutter.dev/) site.
+- Fetch latest source code from master branch.
+
+```console
+rajput-hemant@arch:~$ git clone https://github.com/rajput-hemant/shop-app
+```
+
+- Run the app with Android Studio or VS Code. Or the command line:
+
+```console
+rajput-hemant@arch:~$ flutter pub get
+rajput-hemant@arch:~$ flutter run
+```
---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MyShop',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('MyShop'),
+      ),
+      body: const Center(
+        child: Text('Let\'s build a shop!'),
+      ),
+    );
+  }
+}
