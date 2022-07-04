1b114d315310c0560393cd8e7837fc0539203199
rajput-hemant <hemant.rajput_cs20@gla.ac.in>
2022-07-04 12:57:42 +0530

    added body to display some details of a product
    Signed-off-by: rajput-hemant <hemant.rajput_cs20@gla.ac.in>

--- lib/screens/product_detail_screeen.dart
+++ lib/screens/product_detail_screeen.dart
@@ -5,6 +5,7 @@ import '../providers/products.dart';
 
 class ProductDetailScreen extends StatelessWidget {
   static const routeName = '/product-detail';
+  const ProductDetailScreen({Key? key}) : super(key: key);
   // final String title;
 
   // const ProductDetailScreen(this.title);
@@ -15,8 +16,41 @@ class ProductDetailScreen extends StatelessWidget {
     final loadedProduct =
         Provider.of<Products>(context, listen: false).findByID(productId);
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: Text(loadedProduct.title),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                loadedProduct.imageURL,
+                fit: BoxFit.fitWidth,
+              ),
+            ),
+            const SizedBox(height: 20),
+            Text(
+              '₹ ${loadedProduct.price}',
+              style: const TextStyle(
+                color: Colors.grey,
+                fontSize: 24,
+              ),
+            ),
+            const SizedBox(height: 20),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              width: double.infinity,
+              child: Text(
+                loadedProduct.description,
+                textAlign: TextAlign.center,
+                softWrap: true,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
