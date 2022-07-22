8fe4c858776a502dc31405c4dd4b4f8b9622a577
rajput-hemant <hemant.rajput_cs20@gla.ac.in>
2022-07-22 20:44:17 +0530

    added fetchProducts method to fetch products from firebase rtdb added CircularProgressIndicator to show progress indicator instead of ProductOverviewScreen until get/fetch request finishes
    Signed-off-by: rajput-hemant <hemant.rajput_cs20@gla.ac.in>

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../providers/cart.dart';
-// import '../providers/products.dart';
+import '../providers/products.dart';
 import 'cart_screen.dart';
 import '../widgets/app_drawer.dart';
 import '../widgets/badge.dart';
@@ -22,6 +22,28 @@ class ProductOverviewScreen extends StatefulWidget {
 
 class _ProductOverviewScreenState extends State<ProductOverviewScreen> {
   var showFavoutiteOnly = false;
+  var _isInit = true;
+  var _isLoading = false;
+
+  @override
+  void initState() {
+    // Provider.of<Products>(context, listen: false).fetchProducts();
+    // Future.delayed(Duration.zero).then(
+    //     (_) => Provider.of<Products>(context, listen: false).fetchProducts());
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    if (_isInit) {
+      setState(() => _isLoading = true);
+      Provider.of<Products>(context).fetchProducts().then((_) {
+        setState(() => _isLoading = false);
+      });
+    }
+    _isInit = false;
+    super.didChangeDependencies();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -68,7 +90,11 @@ class _ProductOverviewScreenState extends State<ProductOverviewScreen> {
         ],
       ),
       drawer: const AppDrawer(),
-      body: ProductsGrid(showFavoutite: showFavoutiteOnly),
+      body: _isLoading
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : ProductsGrid(showFavoutite: showFavoutiteOnly),
     );
   }
 }
