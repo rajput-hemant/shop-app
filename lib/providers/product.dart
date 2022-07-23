8f4cb0e8cbcbcbd55edfa67c4cf3c80e7973bba3
rajput-hemant <hemant.rajput_cs20@gla.ac.in>
2022-07-24 01:20:21 +0530

    refactored toggleFavouriteStatus method to return Future<void>, and send a patch request to firebase rtdb to update favourite status of a product
    Signed-off-by: rajput-hemant <hemant.rajput_cs20@gla.ac.in>

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
 
 class Product with ChangeNotifier {
   final String id, title, description;
@@ -15,8 +18,28 @@ class Product with ChangeNotifier {
     this.isFavourite = false,
   });
 
-  void toggleFavouriteStatus() {
+  void _setFavValue(bool newValue) {
+    isFavourite = newValue;
+    notifyListeners();
+  }
+
+  Future<void> toggleFavouriteStatus() async {
+    final oldStatus = isFavourite;
     isFavourite = !isFavourite;
     notifyListeners();
+    final url = Uri.https(
+        'shop-app-z-default-rtdb.firebaseio.com', '/products/$id.json');
+    try {
+      final response = await http.patch(
+        url,
+        body: 
+        json.encode({
+          'isFavourite': isFavourite,
+        }),
+      );
+      if (response.statusCode >= 400) _setFavValue(oldStatus);
+    } catch (e) {
+      _setFavValue(oldStatus);
+    }
   }
 }
