be52e3bb74d463fe08c6cfce2c87250964d29b2e
rajput-hemant <hemant.rajput_cs20@gla.ac.in>
2022-07-29 12:19:22 +0530

    created SplashScreen
    Signed-off-by: rajput-hemant <hemant.rajput_cs20@gla.ac.in>

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          'Loading...',
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
